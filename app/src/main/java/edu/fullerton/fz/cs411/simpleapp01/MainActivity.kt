54f557a8237be368dfa26f57bf8935b56f89fb0d
Frank Zarrinkelk <fzarrinkelk@hotmail.com>
2024-09-13 11:00:39 -0700

    added image processing

--- app/src/main/java/edu/fullerton/fz/cs411/simpleapp01/MainActivity.kt
+++ app/src/main/java/edu/fullerton/fz/cs411/simpleapp01/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.fullerton.fz.cs411.simpleapp01
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var decreaseButton: Button
     private lateinit var increaseButton: Button
     private lateinit var counterView: TextView
+    private lateinit var manImage: ImageView
+    private lateinit var toggleGlassesButton: Button
 
+    private var glassesOn = false
     private var counter = -99
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +43,23 @@ class MainActivity : AppCompatActivity() {
             updateCounterView()
         }
 
+        toggleGlassesButton = findViewById(R.id.toggle_glasses)
+        toggleGlassesButton.setOnClickListener {
+            this.glassesOn = !this.glassesOn
+            this.updateManImage()
+        }
+
+        manImage = findViewById(R.id.man_with_hat_id)
+        manImage.setImageDrawable(getDrawable(R.drawable.man_with_hat))
+    }
+
+    private fun updateManImage() {
+        val newImageId: Int = if (glassesOn) {
+            R.drawable.man_with_glasses
+        } else {
+            R.drawable.man_with_hat
+        }
+        this.manImage.setImageResource(newImageId)
     }
 
     private fun updateCounterView() {
