a2d577b2c38f194df475f995b71df104259d45d0
Frank Zarrinkelk <fzarrinkelk@hotmail.com>
2024-09-13 13:06:48 -0700

    Code with error

--- app/src/main/java/edu/fullerton/fz/cs411/simpleapp01/MainActivity.kt
+++ app/src/main/java/edu/fullerton/fz/cs411/simpleapp01/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var counterView: TextView
     private lateinit var manImage: ImageView
     private lateinit var toggleGlassesButton: Button
+    private lateinit var errorButton: Button
 
     private var glassesOn = false
     private var counter = -99
@@ -48,9 +49,74 @@ class MainActivity : AppCompatActivity() {
             this.glassesOn = !this.glassesOn
             this.updateManImage()
         }
-
         manImage = findViewById(R.id.man_with_hat_id)
         manImage.setImageDrawable(getDrawable(R.drawable.man_with_hat))
+
+        errorButton = findViewById(R.id.error_button)
+        errorButton.setOnClickListener {
+            x()
+        }
+    }
+
+    private fun x() {
+        var x1 = 1
+        y()
+        // val errorButton2: Button = findViewById(R.id.main)
+//        errorButton2.setOnClickListener {
+//            println("")
+//        }
+        z(30)
+        a()
+    }
+
+    private class myException: Exception() {
+
+    }
+
+    private fun a() {
+        var a1 = 5
+        var a2 = 10
+        for (n in 0..a1) {
+            val nn = n * 10
+            println("${n}")
+            for (m in 0..a2) {
+                println("the value of m is ${m}")
+            }
+            try {
+                try {
+                    val p: Int = 3
+                    throw Exception()
+                }
+                catch (e1: Exception) {
+                    println("exception")
+                }
+                try {
+                    throw CharacterCodingException()
+                }
+                catch (e2: Exception)
+                {
+                    println("$e2")
+                }
+                throw myException()
+            } catch (e: Exception) {
+                println("an exception happened $e")
+            }
+            catch (e: myException) {
+                println()
+            }
+        }
+    }
+
+    private fun z(p: Int): String {
+        var z1 = 2
+        println("I am in z and the value of my variable is ${z1 + p}")
+        return "done"
+    }
+
+    private fun y() {
+        println("I am in y")
+        val f = z(20)
+        println(f)
     }
 
     private fun updateManImage() {
